d947da4674735c4d609084e3443205f2a80366a8
kifhok1 <kifhok@gmail.com>
2025-12-14 22:56:11 +0300

    удаление файлов из папки bin

--- obj/Debug/net9.0/VKR.AssemblyInfo.cs
+++ obj/Debug/net9.0/VKR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VKR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856fe6b9c1cdd4272d7a976b1c21dd9e99841b1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VKR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VKR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
