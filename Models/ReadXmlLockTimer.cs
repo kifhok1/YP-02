82c21cc65eebedff02c07302313787cf71e699ce
kifhok1 <kifhok@gmail.com>
2025-12-16 00:28:27 +0300

    Создание xml файла и функции для её чтения

---
+++ Models/ReadXmlLockTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace VKR.Models;
+
+public static class ReadXmlLockTimer
+{
+    public static int ReadConfigFile()
+    {
+            // Загрузка XML-документа
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("./InactivityLockConfiguration.xml");
+            
+            // Извлечение элемента и его значения
+            XmlElement timeoutElement = doc.DocumentElement;
+            
+            int configTime = Convert.ToInt32(timeoutElement.GetAttribute("InactivityTimeoutSeconds"));
+            
+            return configTime;
+    }
+}
